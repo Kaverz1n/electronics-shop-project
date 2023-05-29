43108063564705ba17fef463bd02b4ab52cc9801
Dmitry <dima.captan@yandex.ru>
2023-05-29 17:23:51 +0300

    replace one line in __repr__

--- src/phone.py
+++ src/phone.py
@@ -22,7 +22,8 @@ class Phone(Item):
             return self.quantity + other.quantity
 
     def __repr__(self) -> str:
-        return f'{self.__class__.__name__}{self.name, self.price, self.quantity, self.number_of_sim}'
+        return f'{self.__class__.__name__}' \
+               f'{self.name, self.price, self.quantity, self.number_of_sim}'
 
     def __check_number_of_sim(self, value) -> int:
         '''
